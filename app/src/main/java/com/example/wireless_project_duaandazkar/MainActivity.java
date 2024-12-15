675d8cec18f2e4f5eac9955c6e72041a35773a57
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2024-12-15 13:25:23 +0200

    Initial commit

---
+++ app/src/main/java/com/example/wireless_project_duaandazkar/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.wireless_project_duaandazkar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private CategoryAdapter adapter;
+    private List<String> categoryList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        //categories
+        categoryList = new ArrayList<>();
+        categoryList.add("Dua after Salah");
+        categoryList.add("Morning Azkar");
+        categoryList.add("Evening Azkar");
+        categoryList.add("Daily Essential Dua");
+        categoryList.add("40 Dua begins with 'Rabbana'");
+        categoryList.add("Ruquiya (Audio Only)");
+        categoryList.add("Missed Rak’ah Procedure");
+        categoryList.add("Hajj & Umrah");
+
+        //adapter
+        adapter = new CategoryAdapter(categoryList, this::onCategoryClick);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void onCategoryClick(String category) {
+        // testing
+        Toast.makeText(this, "Selected: " + category, Toast.LENGTH_SHORT).show();
+
+        //here what should app do after click?
+    }
+}
